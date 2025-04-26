a71282cc70ab27b99a4f2a66fca82c71072a3510
Swapneel14 <sarkarswapneel14@gmail.com>
2025-04-26 22:02:58 +0530

    Update endsem_8.c

--- Second Sem/2022/endsem_8.c
+++ Second Sem/2022/endsem_8.c
@@ -1,1 +1,30 @@
+#include<stdio.h>
+typedef struct complex{
+    int real;
+    int img;
 
+};
+void add(int x1,int y1,int x2,int y2){
+int r=x1+x2;
+int c= y1+y2;
+
+if(c>0){
+    printf("Final answer=%d + %di",r,c);
+}
+else{
+    int x=-c;
+    printf("Final answer=%d - %di",r,x);
+}
+}
+void main(){
+    struct complex n1,n2;
+    printf("Enter real part of first complex");
+    scanf("%d",&n1.real);
+    printf("Enter img part of first complex");
+    scanf("%d",&n1.img);
+    printf("Enter real part of second complex");
+    scanf("%d",&n2.real);
+    printf("Enter img part of 2nd complex");
+    scanf("%d",&n2.img);
+    add(n1.real,n1.img,n2.real,n2.img);
+}
