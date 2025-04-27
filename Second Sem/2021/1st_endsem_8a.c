d65785160af6cfe75d58d288fc53ab9cac5c43bf
Swapneel14 <sarkarswapneel14@gmail.com>
2025-04-27 23:09:41 +0530

    Update 1st_endsem_8a.c

--- Second Sem/2021/1st_endsem_8a.c
+++ Second Sem/2021/1st_endsem_8a.c
@@ -1,1 +1,29 @@
-
+#include<stdio.h>//Compare 2 files//
+void main(){
+    FILE *f;
+    FILE *g;
+    char c,d;
+    f= fopen("file1.txt","r");
+    g= fopen("file2.txt","r");
+    while(c!=EOF&& d!=EOF){
+        c=fgetc(f);
+        d=fgetc(g);
+        if (c!=d)
+        {
+            printf("different files");
+            fclose(f);
+            fclose(g);
+            return;
+        }
+        
+    }
+    if(c!=EOF||d!=EOF){
+        printf("different files");
+        fclose(f);
+        fclose(g);
+        return; 
+    }
+    printf("identical files");
+        fclose(f);
+        fclose(g);
+}
