42c81493f432af505f56c9464f92d008662f0091
Swapneel14 <sarkarswapneel14@gmail.com>
2025-04-27 15:26:48 +0530

    Update endsem_3a.c

--- Second Sem/2023/endsem_3a.c
+++ Second Sem/2023/endsem_3a.c
@@ -1,1 +1,12 @@
-
+//example of call by reference//
+#include<stdio.h>
+void change(int *x){
+    *x=100;
+}
+void main(){
+    int x=10;
+    change(&x);
+    printf("%d",x);
+}
+//output will be 100//as in Call by Reference the changes in the function will 
+//modify the original variable//
